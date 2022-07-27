a3bb56bd541db43f1a6b9245f62e0d0e5e834714
HajerBchn <hr.bouchenine@gmail.com>
2022-07-27 20:12:59 +0100

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Hadjer on 27/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
